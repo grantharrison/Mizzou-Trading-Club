e21fd7010e87e703c875ce64f9b909847afa52b4
grantharrison <gthgg6@mail.missouri.edu>
2018-10-17 14:21:21 -0500

    Added Basics

--- PythonBasics/basics.py
+++ PythonBasics/basics.py
@@ -1,1 +1,83 @@
+'''Intro to Python'''
+
+# Python Math
+
+x = 6
+z = 3
+v = 0
+
+v = x + z #Addition
+print(v)
+v = x - z #Subtraction
+print(v)
+v = x * z #Multiplication
+print(v)
+v = x / z #Division
+print(v)
+v = x**3 #Exponential
+print(v)
+
+# IF Statement
+i = 20
+j = 10
+
+if i > j:
+    print ('This means that i must be greater than j')
+    print (i)
+
+# WHILE Loop
+
+x = 5
+z = 0
+
+while x > z:
+    print('True')
+    z = z + 1
+
+# FOR Loop
+
+c = 0
+
+for c in range(5):
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
